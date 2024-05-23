ad11fcb4be8c21025da19085de8e2ef66380b757
Constantin <92950536+ConstantinNshuti@users.noreply.github.com>
2024-05-24 00:14:16 +0200

    View manipulation with ejs Tags <% %>

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view
+
+app.set('view engine', 'ejs');
+
 //listen for request
 const port = 3000;
 app.listen(port, ()=>{
@@ -10,11 +14,19 @@ app.listen(port, ()=>{
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname})
+
+    const blogs = [ 
+
+    ];
+    res.render('index', { title: 'Home', blogs: blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about' , { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res)=>{
+    res.render('create', { title: 'Create New Blog' });
 });
 
 app.use((req, res) => {
